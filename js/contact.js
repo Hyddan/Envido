f8c18c868df29d56c215fdcff219b2bed537fa47
Daniel Hedenius <daniel.hedenius@gmail.com>
2013-07-16 18:09:00 +0200

    Added Facebook like button

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+﻿window.Envido.Contact = (function(Contact) {
+	Contact.loadDependencies = function () {
+		Envido.loadScript('//connect.facebook.net/en_US/all.js#xfbml=1', null);
+	};
+	
+	Contact.initialize = function() {
+	};
+	
+	Contact.loadDependencies();
+	
+	return Contact;
+}(window.Envido.Contact || {}));
+
+Envido.selectedMenuItem = null;
+Envido.ready(Envido.Contact.initialize);
